794b496f1635d5345e79d9c03abf1ad031a4e429
kyle java <42562960+kylejava@users.noreply.github.com>
2022-02-26 10:49:56 -0800

    Created FireBase Keys

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World!!!!")
+from flask import Flask, render_template, flash, request, redirect, url_for, session
+import json
+import requests
+app = Flask(__name__)
+
+
+@app.route('/' , methods = ['GET' , 'POST'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/user_table' , methods = ['GET' , 'POST'])
+def user_table():
+    return render_template('user_table.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
